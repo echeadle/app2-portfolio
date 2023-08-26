6fa47a7fc3c416e1b493f0b3528083327a33dffb
echeadle <echeadle@gmail.com>
2023-08-26 16:52:29 -0600

    Day 21: Add descrption below and not in the colums.

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ with col2:
       data mining to gain business insights with the Australian Rapid Intelligence.
  """
     st.info(content)
+
+
+content2 = """ Below you can find some of the apps I have built in Python. Feel to contact me!
+"""
+st.write(content2)
